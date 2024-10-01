72b643efa8ef6dcf7919cfc2fff7d92a51ddbe0e
Yuri Peixoto <yuripeixoto@gmail.com>
2024-10-01 15:17:21 -0400

    feat: set up personalized exception classes

---
+++ src/Exception/ClientException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Exception;
+
+use Exception;
+
+class ClientException extends Exception
+{
+    // You can add custom methods or properties if needed
+}
--- src/Middleware/JsonBodyParserMiddleware.php
+++ src/Middleware/JsonBodyParserMiddleware.php
@@ -2,6 +2,26 @@
 
 namespace App\Middleware;
 
-class JsonBodyParserMiddleware
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+class JsonBodyParserMiddleware implements MiddlewareInterface
 {
+    public function process(Request $request, RequestHandler $handler): Response
+    {
+        $contentType = $request->getHeaderLine('Content-Type');
+
+        if (strstr($contentType, 'application/json')) {
+            $contents = json_decode(file_get_contents('php://input'), true);
+            if (json_last_error() === JSON_ERROR_NONE) {
+                $request = $request->withParsedBody($contents);
+            } else {
+                throw new \RuntimeException('Malformed JSON in request body');
+            }
+        }
+
+        return $handler->handle($request);
+    }
 }
